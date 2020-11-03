322d8e30b14114b2de0ed72ab333cb047e771eb0
Dan Moore <danielmoorerealperson@gmail.com>
2020-11-03 05:14:24 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,16 +1,16 @@
 #require 'pry'
 
 def generate_star_date
-  star_date = (rand(100000) + 400000) / 10.0
+  (rand(100000) + 400000) / 10.0
 end
 
 
 def state_log(star_date)
-star_date.to_s
   "Captain's Log, star date #{star_date}."
 end
 
 def engage
+  generate_star_date
   puts state_log(star_date)
   date = generate_star_date
 end
